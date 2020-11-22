94dd2dc6b5fd534b9b14382ce07b9057917ff1f6
sonali-sarkar <71331392+sonali-sarkar@users.noreply.github.com>
2020-11-22 18:09:35 +0530

    Create create_table.py
    creating a dynamoDB table using lambda

---
+++ create_table.py
@@ -0,0 +1,35 @@
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.create_table(
+        TableName='Movies',
+        KeySchema=[
+            {
+                'AttributeName': 'year',
+                'KeyType': 'HASH'  # Partition key
+            },
+            {
+                'AttributeName': 'title',
+                'KeyType': 'RANGE'  # Sort key
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'year',
+                'AttributeType': 'N'
+            },
+            {
+                'AttributeName': 'title',
+                'AttributeType': 'S'
+            },
+
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 10,
+            'WriteCapacityUnits': 10
+        }
+    )
+ 
