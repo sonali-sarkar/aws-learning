8c914525fca6598e09aa1a5afd7acca50021093e
sonali-sarkar <71331392+sonali-sarkar@users.noreply.github.com>
2020-11-23 10:53:55 +0530

    Create delete_item.py

---
+++ delete_item.py
@@ -0,0 +1,13 @@
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table('Movies')
+    response = table.delete_item(
+       Key={
+            'year': 2020,
+            'title': 'Panga'
+        }
+    )
